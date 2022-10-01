5a2f9bbf4717483ac6428df66f650fd606ea19e3
TatiynaLN <tania.cis2011@yandex.ru>
2022-10-01 19:28:32 +0300

    Исправления

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  if (min < 0 || min > max) {
+    return NaN;
+  }
+
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 getRandomInt();
 
-function checkMaxLengthStr (comment, maxLength){
-  if(comment.value.length <= maxLength){
+function checkMaxLengthStr(str, maxLength) {
+  if (str.length <= maxLength) {
     return true;
   }
   return false;
