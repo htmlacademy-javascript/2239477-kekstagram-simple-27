e423cef4dc5755b02c17a30b866d2124f2afec0a
TatiynaLN <tania.cis2011@yandex.ru>
2022-10-03 12:41:21 +0300

    Правки

--- js/main.js
+++ js/main.js
@@ -2,18 +2,18 @@ function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
 
-  if (min < 0 || min > max) {
+  if (min < 0 || typeof min !== 'number' || typeof max !== 'number') {
     return NaN;
+  }
+  if (min > max) {
+    min = max;
   }
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
 getRandomInt();
 
-function checkMaxLengthStr(str, maxLength) {
-  if (str.length <= maxLength) {
-    return true;
-  }
-  return false;
-}
-checkMaxLengthStr();
+const checkLengthStr = (str, lengthStr) => str.length <= lengthStr;
+
+checkLengthStr();
