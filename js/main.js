6a09a40d17b85a34f9e9ae85b343553a9243e9e6
TatiynaLN <tania.cis2011@yandex.ru>
2022-10-05 17:41:19 +0300

    4.15. Больше деталей

--- js/main.js
+++ js/main.js
@@ -13,8 +13,27 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-getRandomInt();
-
 const checkLengthStr = (str, lengthStr) => str.length <= lengthStr;
 
 checkLengthStr();
+
+const createsObject = (num) => {
+  const attributesImg = {
+    'id': num,
+    'url': `photos/${num}.jpg`,
+    'description': `Фото - ${num}`,
+    'likes': getRandomInt(15, 200),
+    'comments': getRandomInt(0, 200)
+  };
+  return attributesImg;
+};
+
+const images = [];
+
+function arrPush(numMin, numMax, arr, arrValue) {
+  for (let i = numMin; i <= numMax; i++) {
+    arr.push(arrValue(i));
+  }
+}
+
+arrPush(1, 25, images, createsObject);
