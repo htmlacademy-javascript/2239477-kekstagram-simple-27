2eb06d7f2b0ebf9c589be00152bfb22235ae1e66
TatiynaLN <tania.cis2011@yandex.ru>
2022-10-12 10:55:42 +0300

    модули

---
+++ js/create-img-description.js
@@ -0,0 +1,17 @@
+import {getRandomInt} from './util.js';
+import {getRandomString} from './get-random-string.js';
+
+const getObjectDescription = (num) => {
+  const string = 'Lorem ipsum dolor sit amet consectetur adipisicing elit.';
+
+  const attributesImg = {
+    'id': num,
+    'url': `photos/${num}.jpg`,
+    'description': getRandomString(string),
+    'likes': getRandomInt(15, 200),
+    'comments': getRandomInt(0, 200)
+  };
+  return attributesImg;
+};
+
+export {getObjectDescription};
